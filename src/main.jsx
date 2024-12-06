266c95c57d176005fa32d0fefb911267dce3e0ab
Jaswanth Kandati <jaswanth.kandati@gmail.com>
2024-12-06 13:29:02 +0530

    removed clerk as per chatgpt

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,9 +13,7 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> */}
       <App />
-      {/* </ClerkProvider> */}
     </Router>
   </StrictMode>
 );
