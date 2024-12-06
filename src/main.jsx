e1b1eedeedb69800a0c6c47030c40a6767726967
Jaswanth Kandati <jaswanth.kandati@gmail.com>
2024-12-06 13:10:56 +0530

    adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -14,9 +14,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
-      </ClerkProvider>
+      {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> */}
+      <App />
+      {/* </ClerkProvider> */}
     </Router>
   </StrictMode>
 );
